3c25cbbba396b3c294335655143aac82c6590c58
richardth06 <richardth06@gmail.com>
2010-09-27 18:50:27 -0700

    Updated ListADT

--- ListADT.java
+++ ListADT.java
@@ -26,3 +26,29 @@ public interface ListADT<E> extends Iterable<E> {
 //  Returns the last Object in the list, but does not remove it.
 //  Returns null if the list is empty.
     public E peekLast();
+
+//  Removes the first instance of thespecific Object obj from the list, if it exists.
+//  Returns true if the Object obj was found and removed, otherwise false
+    public boolean remove(E obj);
+
+//  The list is returned to an empty state.
+    public void makeEmpty();
+
+//  Returns true if the list contains the Object obj, otherwise false
+    public boolean contains(E obj);
+
+//  Returns true if the list is empty, otherwise false
+    public boolean isEmpty();
+
+//  Returns true if the list is full, otherwise false
+    public boolean isFull();
+
+//  Returns the number of Objects currently in the list.
+    public int size();
+
+//  Returns an Iterator of the values in the list, presented in
+//  the same order as the list.
+    public Iterator<E> iterator();
+
+}
+
